38f93631eb2e1d44d4aca103e524b2d1cd3c2e9c
joaords98-ctrl <joaords98@gmail.com>
2026-06-06 04:26:12 -0300

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RachadometroCuritiba from "./RachadometroCuritiba.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RachadometroCuritiba />
-  </React.StrictMode>
-);
